beb72818b7c2169f677a70a40792b83aa8a0aecd
Bhanu Arora <mrbhanuarora@gmail.com>
2022-08-12 23:29:50 +0530

    rss-feeder

--- backend/rss-feeder/src/utils/db.config.ts
+++ backend/rss-feeder/src/utils/db.config.ts
@@ -1,6 +1,6 @@
 const sequelize = require("sequelize");
 const {Sequelize} = sequelize;
-export let seq = new Sequelize("test","postgres","",{
+export let seq = new Sequelize("test","postgres","a+b+c+d=e+f",{
     host:"localhost",
     dialect:"postgres"
 });
